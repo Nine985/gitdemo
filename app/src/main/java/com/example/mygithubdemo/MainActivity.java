06a0593de85e73e7038851f2b03db3391ed9672b
yj <384133190@qq.com>
2021-02-23 22:12:12 +0800

    第4次提交

--- app/src/main/java/com/example/mygithubdemo/MainActivity.java
+++ app/src/main/java/com/example/mygithubdemo/MainActivity.java
@@ -23,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String a="junge   12345";
+                String b="junge   12345789";
+                String c="junge   12345456*";
+                String d="junge   12345123-";
+                String e="junge   12345dashabi";
             }
         });
     }

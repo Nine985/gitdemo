2bf4fbf0357652b8ce7020b1a721a102039bfc82
yj <384133190@qq.com>
2021-02-23 22:07:27 +0800

    第3次提交

--- app/src/main/java/com/example/mygithubdemo/MainActivity.java
+++ app/src/main/java/com/example/mygithubdemo/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.mygithubdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+
+
+    }
+
+    private void initView() {
+        TextView tv = findViewById(R.id.tv);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String a="junge   12345";
+            }
+        });
     }
 }
